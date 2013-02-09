177b31ce70bf181e8e6b2bc074aad407bc48567c
Andreas Bresser <a.bresser@greyrook.com>
2013-02-09 18:15:36 +0100

    compiled for robotlegs 2.0.0b4

---
+++ build.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Adobe Flex Build system - connect to flex remote compile shell and run build
+
+from rook.de import flex
+import time
+import os
+import shutil
+
+sdk = flex.SDK('4.6.0 build 23201')
+
+sdk.swc('robotlegs2-starling-viewmap-2.0.0b4', 
+        output='bin/robotlegs2-starling-viewmap-2.0.0b4.swc', 
+        requires=[l[:-4] for l in os.listdir('libs/') if l.endswith('swc')],
+        #args=['-static-link-runtime-shared-libraries=true', '-debug'])
+        )
